58b84ef7bc5c4776b5692927cd52b20e03593684
しんじょーくん <50821568+ShinjouKun@users.noreply.github.com>
2022-12-13 21:03:00 +0900

    とりあえずテストで使いそうなものを追加

---
+++ assets/TsCode/Acter/TankBase.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+//戦車の基礎クラス
+@ccclass('TankBase')
+export class TankBase extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
